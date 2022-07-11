bf625749268add0fc737531222747b2937f8389f
Raúl Ruiz <raul.ruiz.bolivar@gmail.com>
2022-07-11 17:37:22 +0200

    borrando cosas

--- config/db.js
+++ config/db.js
@@ -2,5 +2,3 @@ const mongoose = require( 'mongoose' )
 
 
 mongoose.connect( process.env.MONGO_URL )
-
-//mongodb+srv://raulruizbolivar:passwordmongo@chat.ovnwl.mongodb.net/?retryWrites=true&w=majority
